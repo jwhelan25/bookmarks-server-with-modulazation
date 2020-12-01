2f69aff8b5aab5d0b27180c65ef58fa933563dcf
Joshua Whelan <josh25whelan@gmail.com>
2020-12-01 13:30:33 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+
 const { NODE_ENV } = require('./config')
+const validateBearerToken = require('./validateBearerToken')
+const errorHandler = require('./errorHandler')
+const bookmarkRouter = require('./routes/bookmark')
 
 const app = express()
 
@@ -16,19 +20,10 @@ app.use(express.json())
 app.use(helmet())
 app.use(cors())
 
-app.get('/', (req,res) => {
-    res.json({message: 'Hello Boilerplate!'})
-})
+app.use(validateBearerToken)
 
-app.use(function errorHandler(error, req, res, next) {
-       let response
-       if (NODE_ENV === 'production') {
-         response = { error: { message: 'server error' } }
-       } else {
-         console.error(error)
-         response = { message: error.message, error }
-       }
-       res.status(500).json(response)
-     })
+app.use(bookmarkRouter)
+
+app.use(errorHandler)
 
 module.exports = app
